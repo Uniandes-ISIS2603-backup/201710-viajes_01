7511dec1cb400fa1622b8c90556c321f80e71fd6
CamiloN <>
2017-03-13 10:48:57 -0500

    Creación del DetailDTO de Vehiculo

---
+++ viajes-01-web/src/main/java/co/edu/uniandes/csw/viajes/dtos/VehiculoDetailDTO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viajes.dtos;
+
+import co.edu.uniandes.csw.viajes.entities.VehiculoEntity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class VehiculoDetailDTO extends VehiculoDTO{
+
+   
+    public VehiculoDetailDTO() {
+        super();
+    }
+
+    public VehiculoDetailDTO(VehiculoEntity entity) {
+        super(entity);
+    }
+
+    @Override
+    public VehiculoEntity toEntity() {
+        VehiculoEntity entity = super.toEntity();
+        return entity;
+    }
+}
