ff2ee00f6db936f986b76360df9e29c280901c3e
Ana Camila Fandiño <ana.fandino@outlook.com>
2017-03-26 10:29:16 -0500

    Basic ToDo's. Removal of comments.

--- viajes-01-logic/src/main/java/co/edu/uniandes/csw/viajes/ejbs/VehiculoLogic.java
+++ viajes-01-logic/src/main/java/co/edu/uniandes/csw/viajes/ejbs/VehiculoLogic.java
@@ -1,9 +1,4 @@
-// TODO: eliminar mensajes por defecto
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package co.edu.uniandes.csw.viajes.ejbs;
 
 import co.edu.uniandes.csw.viajes.entities.VehiculoEntity;
