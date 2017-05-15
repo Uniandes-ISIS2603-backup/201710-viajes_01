14820364e891e2fd80d951970e19907ce2edf224
jp.perez12 <jp.perez12@ISIS2603-0185.sis.virtual.uniandes.edu.co>
2017-05-15 14:52:29 -0500

    usuariologictest2

--- viajes-01-logic/src/test/java/co/edu/uniandes/csw/viajes/test/logic/UsuarioLogicTest.java
+++ viajes-01-logic/src/test/java/co/edu/uniandes/csw/viajes/test/logic/UsuarioLogicTest.java
@@ -18,7 +18,9 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
@@ -114,6 +116,85 @@ public class UsuarioLogicTest
         }
     }
     
+    @Test
+    public void createUsuarioTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        UsuarioEntity entity = factory.manufacturePojo(UsuarioEntity.class);
+        UsuarioEntity result = usuarioLogic.createUsuario(entity);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(result.getId(), entity.getId());
+        Assert.assertEquals(result.getEmail(), entity.getEmail());
+        Assert.assertEquals(result.getLicencia(), entity.getLicencia());
+        Assert.assertEquals(result.getMulta(), entity.getMulta());
+        Assert.assertEquals(result.getNombre(), entity.getNombre());
+        Assert.assertEquals(result.getNumero(), entity.getNumero());
+        Assert.assertEquals(result.getReservas(), entity.getReservas());
+        Assert.assertEquals(result.getTelefono(), entity.getTelefono());
+        Assert.assertEquals(result.getVehiculos(), entity.getVehiculos());
+    }
     
+    @Test
+    public void getUsuariosTest() {
+        List<UsuarioEntity> list = usuarioLogic.getUsuarios();
+        Assert.assertEquals(data.size(), list.size());
+        for (UsuarioEntity entity : list) {
+            boolean found = false;
+            for (UsuarioEntity storedEntity : data) {
+                if (entity.getId().equals(storedEntity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    @Test
+    public void getViajeTest() {
+        UsuarioEntity entity = data.get(0);
+        UsuarioEntity result = usuarioLogic.getUsuario(entity.getId());
+        Assert.assertNotNull(result);
+        Assert.assertEquals(result.getId(), entity.getId());
+        Assert.assertEquals(result.getEmail(), entity.getEmail());
+        Assert.assertEquals(result.getLicencia(), entity.getLicencia());
+        Assert.assertEquals(result.getMulta(), entity.getMulta());
+        Assert.assertEquals(result.getNombre(), entity.getNombre());
+        Assert.assertEquals(result.getNumero(), entity.getNumero());
+        Assert.assertEquals(result.getReservas(), entity.getReservas());
+        Assert.assertEquals(result.getTelefono(), entity.getTelefono());
+        Assert.assertEquals(result.getVehiculos(), entity.getVehiculos());
+    }
+    
+    @Test
+    public void deleteCiudadTest() {
+        UsuarioEntity entity = data.get(0);
+        usuarioLogic.deleteUsuario(entity.getId());
+        UsuarioEntity deleted = em.find(UsuarioEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+    
+    @Test
+    public void updateUsuarioTest() {
+        UsuarioEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        UsuarioEntity pojoEntity = factory.manufacturePojo(UsuarioEntity.class);
+        pojoEntity.setId(entity.getId());
+
+        usuarioLogic.updateUsuario(pojoEntity);
+
+        UsuarioEntity resp = em.find(UsuarioEntity.class, entity.getId());
+        Assert.assertNotNull(resp);
+        Assert.assertEquals(resp.getId(), entity.getId());
+        Assert.assertEquals(resp.getEmail(), entity.getEmail());
+        Assert.assertEquals(resp.getLicencia(), entity.getLicencia());
+        Assert.assertEquals(resp.getMulta(), entity.getMulta());
+        Assert.assertEquals(resp.getNombre(), entity.getNombre());
+        Assert.assertEquals(resp.getNumero(), entity.getNumero());
+        Assert.assertEquals(resp.getReservas(), entity.getReservas());
+        Assert.assertEquals(resp.getTelefono(), entity.getTelefono());
+        Assert.assertEquals(resp.getVehiculos(), entity.getVehiculos());
+    }
 
 }
+
+
