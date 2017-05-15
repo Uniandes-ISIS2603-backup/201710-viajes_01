0a05b114a1c880ddca5575e71f44947775ce279d
jp.perez12 <jp.perez12@ISIS2603-0185.sis.virtual.uniandes.edu.co>
2017-05-15 10:33:33 -0500

    usuariologictest1

--- viajes-01-logic/src/test/java/co/edu/uniandes/csw/viajes/test/logic/UsuarioLogicTest.java
+++ viajes-01-logic/src/test/java/co/edu/uniandes/csw/viajes/test/logic/UsuarioLogicTest.java
@@ -5,10 +5,115 @@
  */
 package co.edu.uniandes.csw.viajes.test.logic;
 
+import co.edu.uniandes.csw.viajes.ejbs.UsuarioLogic;
+import co.edu.uniandes.csw.viajes.entities.UsuarioEntity;
+import co.edu.uniandes.csw.viajes.persistence.UsuarioPersistence;
+import java.util.List;
+import java.util.ArrayList;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author pj.alvarado10
  */
-public class UsuarioLogicTest {
+public class UsuarioLogicTest
+{
+
+    public static final String DEPLOY = "Prueba";
+
+    /**
+     * @generated
+     */
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
+                .addPackage(UsuarioEntity.class.getPackage())
+                .addPackage(UsuarioLogic.class.getPackage())
+                .addPackage(UsuarioPersistence.class.getPackage())
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource("META-INF/beans.xml", "beans.xml");
+    }
+        /**
+     * @generated
+     */
+    @Inject
+    private UsuarioLogic usuarioLogic;
+
+    /**
+     * @generated
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * @generated
+     */
+    @Inject
+    UserTransaction utx;
+
+    /**
+     * Configuración inicial de la prueba.
+     *
+     * @generated
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     * @generated
+     */
+    private void clearData() {
+        em.createQuery("delete from UsuarioEntity").executeUpdate();
+    }
+
+    /**
+     * @generated
+     */
+    private List<UsuarioEntity> data = new ArrayList<UsuarioEntity>();
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     * @generated
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            PodamFactory factory = new PodamFactoryImpl();
+            UsuarioEntity entity = factory.manufacturePojo(UsuarioEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
     
+    
+
 }
